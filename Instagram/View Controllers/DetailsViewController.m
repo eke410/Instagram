d0a5de238a17278ce0f6fc361e9b2369823b72ec
eke410 <elizabethke100@gmail.com>
2021-07-08 16:00:33 -0400

    dded ability to like post from DetailsViewController

--- Instagram/View Controllers/DetailsViewController.m
+++ Instagram/View Controllers/DetailsViewController.m
@@ -16,6 +16,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *captionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *createdAtLabel;
 @property (weak, nonatomic) IBOutlet UILabel *likesCountLabel;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
 
 @end
 
@@ -35,6 +36,46 @@
     self.authorLabel.text = self.post.author.username;
     self.createdAtLabel.text = [self.post.createdAt formattedDateWithFormat:@"MMMM d, yyyy · h:mm a"];
     self.likesCountLabel.text = [[self.post.likeCount stringValue] stringByAppendingString:@" likes"];
+    
+    if ([self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId]) {
+        [self updateLikeButtonToLiked];
+    } else {
+        [self updateLikeButtonToUnliked];
+    }
+}
+
+- (IBAction)likePost:(id)sender {
+    if (![self.post.usersWhoLiked containsObject:PFUser.currentUser.objectId]) {
+        NSLog(@"Liking post");
+        self.post.usersWhoLiked = [self.post.usersWhoLiked arrayByAddingObject:PFUser.currentUser.objectId];
+        self.post.likeCount = @([self.post.likeCount intValue] + 1);
+        [self.post saveInBackground];
+        
+        self.likesCountLabel.text = [[self.post.likeCount stringValue] stringByAppendingString:@" likes"];
+        [self updateLikeButtonToLiked];
+    } else {
+        NSLog(@"Unliking post");
+        NSMutableArray *mutableCopy = [self.post.usersWhoLiked mutableCopy];
+        [mutableCopy removeObject:PFUser.currentUser.objectId];
+        self.post.usersWhoLiked = (NSArray *)mutableCopy;
+        self.post.likeCount = @([self.post.likeCount intValue] - 1);
+        [self.post saveInBackground];
+        
+        self.likesCountLabel.text = [[self.post.likeCount stringValue] stringByAppendingString:@" likes"];
+        [self updateLikeButtonToUnliked];
+    }
+}
+
+- (void)updateLikeButtonToLiked {
+    UIImage *image = [UIImage systemImageNamed:@"heart.fill" withConfiguration:[UIImageSymbolConfiguration configurationWithScale:(UIImageSymbolScaleLarge)]];
+    [self.likeButton setImage:image forState:UIControlStateNormal];
+    [self.likeButton setTintColor:[UIColor redColor]];
+}
+
+- (void)updateLikeButtonToUnliked {
+    UIImage *image = [UIImage systemImageNamed:@"heart" withConfiguration:[UIImageSymbolConfiguration configurationWithScale:(UIImageSymbolScaleLarge)]];
+    [self.likeButton setImage:image forState:UIControlStateNormal];
+    [self.likeButton setTintColor:[UIColor blackColor]];
 }
 
 /*
